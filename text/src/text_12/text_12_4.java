69d6f083541cd70c44d3b655f2f8bf96a9e9b53a
张哈大 <3448834662@qq.com>
2024-12-04 22:38:20 +0800

    12.4java

--- text/src/text_12/text_12_4.java
+++ text/src/text_12/text_12_4.java
@@ -4,6 +4,7 @@ public class text_12_4 {
     public static void main(String[]args){
         System.out.println("hello world");
         int num = 10;
+        int  a = 10;
         System.out.println(num);
     }
 }
