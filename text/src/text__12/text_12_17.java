5ea59c558fe9521f62d4a3d9f79f1905cd276301
张哈大 <3448834662@qq.com>
2024-12-17 21:22:33 +0800

    12.17java 继承

---
+++ text/src/text__12/text_12_17.java
@@ -0,0 +1,12 @@
+package text__12;
+
+public class text_12_17 {
+    public static void main(String[] args) {
+        textCook_12_17 c = new textCook_12_17("12","zhangsan","122");
+        textManager_12_17 m = new textManager_12_17("13","lisi","147","7787");
+        c.eat();
+        c.work();
+        m.eat();
+        m.work();
+    }
+}
