65403e9dd4a7f483c4c98262c8b030227d8605b0
张哈大 <3448834662@qq.com>
2025-02-22 22:11:50 +0800

    2.22 java

---
+++ text/src/com/text_25_2_22/Student.java
@@ -0,0 +1,61 @@
+package com.text_25_2_22;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(age, student.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    private String age;
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
