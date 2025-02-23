85b72d956bfd4f5408528e8545ac0ae211cee330
张哈大 <3448834662@qq.com>
2025-02-23 20:56:59 +0800

    2.23 java

---
+++ text/src/com/text_25_2_23/text_23.java
@@ -0,0 +1,37 @@
+package com.text_25_2_23;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class text_23 {
+    public static void main(String[] args) {
+        //int i = Math.absExact(-2);
+        /*long l = System.currentTimeMillis();
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+           arr[i] = i;
+        }
+        int[] num = new int[10];
+        System.arraycopy(arr,0,num,0,10);
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+        long j = System.currentTimeMillis();
+        System.out.println(j-l);*/
+//        Runtime r = Runtime.getRuntime();
+//        int i = r.availableProcessors();
+//        long l = r.maxMemory();
+//        long l1 = r.totalMemory();
+//        long l2 = r.freeMemory();
+//        System.out.println(i);
+//        System.out.println(l/1024/1024);
+//        System.out.println(l1/1024/1024);
+//        System.out.println(l2/1024/1024);
+//        BigInteger b = BigInteger.valueOf(1000);
+//        BigInteger b1 = new BigInteger("10000");
+//        System.out.println(b);
+//        System.out.println(b1);
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
