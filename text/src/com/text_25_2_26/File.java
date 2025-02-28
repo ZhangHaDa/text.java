d8848c96a446c3569b7db3a95627439cb4c795c8
张哈大 <3448834662@qq.com>
2025-02-28 15:39:21 +0800

    2.28 java

--- text/src/com/text_25_2_26/File.java
+++ text/src/com/text_25_2_26/File.java
@@ -1,14 +1,13 @@
 package com.text_25_2_26;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class File {
-    private String filename;
+    private String filename = "./myData.txt";
 
     public File() {
-        this.filename = "./mydata.txt";
+        this.filename = "./myData.txt";
     }
 
     public File(String filename) {
@@ -26,11 +25,10 @@ public class File {
 
     public void write(){}
     String[] fruits = {"apple","banana","pear","orange"};
-    FileWriter fw;
 
     {
         try {
-            fw = new FileWriter(this.filename);
+            FileWriter  fw = new FileWriter(this.filename);
             BufferedWriter bw  = new BufferedWriter(fw);
             for(String elem : fruits){
                 bw.write(elem);
@@ -41,6 +39,26 @@ public class File {
             System.out.println("Save file successfully.");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    public void read(){
+        ArrayList<String> list = new ArrayList<>();
+        String line;
+
+        try {
+            FileReader fr = new FileReader(this.filename);
+            BufferedReader br = new BufferedReader(fr);
+            while((line=br.readLine())!=null){
+                list.add(line);
+            }
+            br.close();
+            fr.close();
+            for (String s : list) {
+                System.out.print(s+"\t");
+            }
+            System.out.println();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
---
+++ text/src/com/text_25_2_28/text_28.java
@@ -0,0 +1,10 @@
+package com.text_25_2_28;
+
+import com.text_25_2_26.File;
+
+public class text_28 {
+    public static void main(String[] args) {
+        File fi = new File();
+        fi.read();
+    }
+}
